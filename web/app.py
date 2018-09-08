7d6aef8bf2c53488389f2f5aba61915b6c81af1f
Jiahui Chen <jiahui.k.chen@gmail.com>
2018-09-08 17:57:36 -0600

    Galileo certificate interaction added.

--- web/app.py
+++ web/app.py
@@ -4,10 +4,17 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 import requests
+from xml.dom.minidom import parseString
+import string
+import random
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+def galileo_cert_auth():
+	data = {'apiLogin':'','apiTransKey':'','providerId':'','transactionId':'30323-random-string-86881'}
+	r = requests.post('https://sandbox-api.gpsrv.com/intserv/4.0/ping', data=payload, cert='galileo148.pem')
+
+@app.route('/', methods=['GET']) 
 def info():
     return "It's running!"
 

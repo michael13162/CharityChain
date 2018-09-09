285a83c7f09371e82e808a27134d50c91769166b
William Li <williamjli@gmail.com>
2018-09-08 20:44:40 -0600

    Added login/registration and http service

--- frontend/src/app/loginscreen/loginscreen.component.ts
+++ frontend/src/app/loginscreen/loginscreen.component.ts
@@ -1,4 +1,6 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NavigationService, AppScreen } from '../services/navigationservice';
+import { HttpService } from '../services/httpservice';
 
 @Component({
   selector: 'login-screen-component',
@@ -7,4 +9,19 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginScreenComponent {
+    username: string;
+    password: string;
+
+    constructor(private navigationService: NavigationService, private httpService: HttpService) {
+    }
+    
+    switchToCreateAccount() {
+        this.navigationService.navigateTo(AppScreen.CreateAccount);
+    }
+
+    login() {
+        this.httpService.login(this.username, this.password).then(() => {
+            this.navigationService.navigateTo(AppScreen.Search);
+        });
+    }
 }

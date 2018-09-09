25777ffdd5a3e3066484518d857bf7c5843bc2c0
William Li <williamjli@gmail.com>
2018-09-08 21:30:56 -0600

    Search Screen and header updates

---
+++ frontend/src/app/profilescreen/profilescreen.component.ts
@@ -0,0 +1,15 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NavigationService } from '../services/navigationservice';
+import { HttpService } from '../services/httpservice';
+import { UserService } from '../services/userservice';
+
+@Component({
+  selector: 'profile-screen-component',
+  templateUrl: './profilescreen.component.html',
+  styleUrls: ['./profilescreen.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProfileScreenComponent {
+    constructor(private navigationService: NavigationService, private httpService: HttpService, private userService: UserService) {
+    }
+}
---
+++ frontend/src/app/searchscreen/searchscreen.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'search-screen-component',
+  templateUrl: './searchscreen.component.html',
+  styleUrls: ['./searchscreen.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SearchScreenComponent {
+    constructor() {
+    }
+}

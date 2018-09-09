1bffa93ce67046bc0e538fab9804a215a5fff366
Jiahui Chen <jiahui.k.chen@gmail.com>
2018-09-09 03:31:25 -0600

    More Galileo methods.

--- web/galileo.py
+++ web/galileo.py
@@ -18,32 +18,54 @@ def get_trans_history(account_id):
 	data = {'apiLogin': api_login,'apiTransKey': api_transKey, 'providerId': provider_id,'transactionId': trans_id(),
 			'accountNo': account_id, 'startDate': '2018-09-08', 'endDate': '2020-05-20'}
 	r = requests.post(url + 'getTransHistory', data=data, cert='galileo148.pem', headers=headers)
-	json = r.json()
+	json_response = r.json()
 
 	trans_history = {}
-	
+	trans_count = 0
+	for transaction in json_response['response_data']['transactions']:
+		merchant = transaction['transaction']['formatted_merchant_desc']
+		amount = transaction['transaction']['amt']
+		time = transaction['transaction']['post_ts']
+		description = transaction['transaction']['details']
+		trans_map = {'merchant': merchant, 'amount': amount, 'time':time, 'description':description}
+		trans_history['transaction' + str(trans_count)] = trans_map
 
-	print(json)
+	json_data = json.dumps(trans_history)
+	print(json_data)
+	return json_data
 
 def test():
-	create_account('Cole')
+	id1 = create_account('grrrrr')
+	id2 = create_account('mooooboooo')
+	print(id1)
+	print(id2)
+
+	print(get_balance(id1))
+
+
+	create_transaction(id1, id2, 10.00, 'lol')
+
+	get_trans_history(id1)
 
 def create_transaction(sender, recipient, amount, description):
 	data = {'apiLogin': api_login,'apiTransKey': api_transKey, 'providerId': provider_id,'transactionId': trans_id(),
 				   'accountNo': sender, 'amount': amount, 'transferToAccountNo': recipient, 'message': description}
 	r = requests.post(url + 'createAccountTransfer', data = data, cert='galileo148.pem', headers=headers)
+	print(r.text)
 
 def create_account(name):
 	data = {'apiLogin': api_login,'apiTransKey': api_transKey, 'providerId': provider_id,'transactionId': trans_id(),
-			   'prodId': product_id, 'firstName': name}
+			   'prodId': product_id, 'firstName': name, 'loadAmount': 100.00}
 	r = requests.post(url + 'createAccount', data=data, cert='galileo148.pem', headers=headers)
 	json = r.json()
 	# returns the generated account ID
 	return json['response_data']['new_account\\1']['pmt_ref_no']
 
+def get_balance(account_id):
+	data = {'apiLogin': api_login, 'apiTransKey': api_transKey, 'providerId': provider_id, 'transactionId': trans_id(),
+			'accountNo': account_id}
+	r = requests.post(url + 'getBalance', data=data, cert='galileo148.pem', headers=headers)
+	return r.json()['response']['response_data']['balance']
+
 if __name__ == '__main__':
-	#galileo_cert_auth()
-	#get_trans_history('0090909', '2018-09-08')
-	get_trans_history('999900089841')
-	#create_transaction('test1', 'test2', 20.00, 'hello')
-	#create_account('test')
+	test()

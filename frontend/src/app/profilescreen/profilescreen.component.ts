36377fbadd7491c3997814d41db7097ce00f49bb
William Li <williamjli@gmail.com>
2018-09-09 06:31:33 -0600

    Fleshed out panel

--- frontend/src/app/profilescreen/profilescreen.component.ts
+++ frontend/src/app/profilescreen/profilescreen.component.ts
@@ -1,5 +1,4 @@
 import { Component, ChangeDetectionStrategy, OnInit, ChangeDetectorRef } from '@angular/core';
-import { NavigationService } from '../services/navigationservice';
 import { HttpService } from '../services/httpservice';
 import { UserService } from '../services/userservice';
 
@@ -12,18 +11,14 @@ import { UserService } from '../services/userservice';
 export class ProfileScreenComponent implements OnInit {
   jsonProfile: any;
 
-  constructor(private cdr: ChangeDetectorRef, private navigationService: NavigationService, private httpService: HttpService, private userService: UserService) {
-  }
-
-  public isCharity() {
-    return this.userService.isCharity();
+  constructor(private cdr: ChangeDetectorRef, private httpService: HttpService, private userService: UserService) {
   }
 
   ngOnInit(): void {
     this.httpService.getCharityInformation(this.userService.getEin()).then(
       (value) => {
         this.jsonProfile = value;
-        this.cdr.markForCheck();
+        this.cdr.detectChanges();
       }
     );
   }
---
+++ frontend/src/app/profilewell/profilewell.component.ts
@@ -0,0 +1,124 @@
+import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'profile-well-component',
+  templateUrl: './profilewell.component.html',
+  styleUrls: ['./profilewell.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProfileWellComponent {
+  @Input() jsonProfile: any;
+  @Input() overlay: boolean;
+  @Output() overlayClosed: EventEmitter<void> = new EventEmitter<void>();
+  @Output() donateClicked: EventEmitter<void> = new EventEmitter<void>();
+
+  constructor() {}
+
+  getOrganizationName(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.referenceOrganization.charityName;
+    }
+  }
+
+  getOrganizationUrl(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.referenceOrganization.charityNavigatorURL;
+    }
+  }
+
+  getRatingImage(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.ratingImage.large;
+    }
+  }
+
+  getAdministrationExpenseRatio(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.administrationExpensesRatio;
+    }
+  }
+
+  getFundraisingEfficiency(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.fundraisingEfficiency;
+    }
+  }
+
+  getLiabilitiesToAssetsRatio(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.liabilitiesToAssetsRatio;
+    }
+  }
+
+  getProgramExpensesGrowth(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.programExpensesGrowth;
+    }
+  }
+
+  getProgramExpensesRatio(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.programExpensesRatio;
+    }
+  }
+
+  getWorkingCapitalRatio(): string {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.financialRating.performanceMetrics.workingCapitalRatio;
+    }
+  }
+
+  auditedFinancialStatus(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.auditedFinancialStatus == "Pass";
+    }
+  }
+
+  boardListStatus(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.boardListStatus == "Pass";
+    }
+  }
+
+  boardMeetingMinutes(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.boardMeetingMinutes == "Pass";
+    }
+  }
+
+  ceoCompensationProcedure(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.ceoCompensationProcedure == "Pass";
+    }
+  }
+
+  compensatesBoard(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.compensatesBoard == "Pass";
+    }
+  }
+
+  form990Status(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.form990Status == "Pass";
+    }
+  }
+
+  independentAudit(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.independentAudit == "Pass";
+    }
+  }
+
+  privacyStatus(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.privacyStatus == "Pass";
+    }
+  }
+
+  reportsCEOAndSalary(): boolean {
+    if (this.jsonProfile) {
+      return this.jsonProfile.charity_info.accountabilityRating.accountabilityTests.reportsCEOAndSalary == "Pass";
+    }
+  }
+}
